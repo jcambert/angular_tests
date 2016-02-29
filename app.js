95c1e99bc8e94f2c8861bf31f62ffa2d8a418bd1
Ambert Jean-Christophe <jc.ambert@free.fr>
2016-02-29 17:06:00 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,49 +1,169 @@
-(function (window,angular) {
+(function (window,angular,$) {
     'use strict';
-    var weberp = window.weberp = angular.module('weberp', []);
+    var weberp = window.weberp = angular.module('weberp', ['ngCookies','ui.router','pascalprecht.translate']);
     
-    weberp.directive('wSidebarMenu', [function () {
+    weberp.constant('LOCALES', {
+        'locales': {
+           // 'ru_RU': 'Русский',
+            'fr_FR': 'Francais'
+        },
+    'preferredLocale': 'fr_FR'
+    })
+
+    weberp.config(['$stateProvider','$urlRouterProvider','$translateProvider','$logProvider',function($stateProvider, $urlRouterProvider,$translateProvider,$log) {
+        $log.debugEnabled(true);
+        
+        /* Translation*/
+        $translateProvider.useMissingTranslationHandlerLog();
+        $translateProvider.useStaticFilesLoader({
+            //prefix: 'resources/locale-',// path to translations files
+            prefix: 'locale-',
+            suffix: '.json'// suffix, currently- extension of the translations
+        });
+        $translateProvider.preferredLanguage('fr_FR');// is applied on first load
+        $translateProvider.useLocalStorage();// saves selected language to localStorage
+        /* /Translation */
+        console.info('WebErp is configured');
+    }]);
+    weberp.run([function(){
+        $(".right_col").css("min-height", $(window).height());
+        $(window).resize(function () {
+            $(".right_col").css("min-height", $(window).height());
+        });
+    }]);
+    
+    
+    
+    
+    weberp.directive('wSidebarMenu', ['$log','$timeout',function ($log,$timeout) {
         return {
             restrict: 'E',
             replace: true,
-            transclude: true,
+            transclude:true,
             $scope:{
                 
             },
-            template:'<ul class="nav side-menu"><li ng-repeat="item in items"><a>{{item.text}}<i class="fa fa-{{item.icon}}"></i></a><w-menu-item items="{{item.items}}"></w-menu-item></li></ul>'
-        };
-    }]);
-
-    weberp.directive('wMenuItem', [function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: true,
-            require: '^wSidebarMenu',
-            $scope:{
-                items:'@'
-            },
-            template:'<ul class="nav" ng-class="{\'child_menu\': items && items.length>0}"><li ng-repeat="item in items"><w-menu-subitem state="{{item.state || \'\'}}" link="{{item.link || \'\'}}">{{item.text}} </w-menu-subitem></li></ul>',
+            controller:function($scope){},
+            template:'<div id="sidebar-menu" class="main_menu_side hidden-print main_menu"><w-menu-section ng-repeat="section in menuSections" ng-transclude><h3>{{section.title}}</h3><ul class="nav side-menu"><li ng-repeat="menu in section.menus"><a><i class="fa fa-{{menu.icon}}"></i>{{menu.text}}<span class="fa fa-chevron-down"></span></a><w-menu-item ></w-menu-item></li></ul></w-menu-section></div>',
             link:function($scope,$element,$attrs){
-                $scope.items=$attrs.items;
+                $timeout(function(){
+                   
+                    $element.find('li ul').slideUp();
+                    $element.find('li').removeClass('active');
+                    
+                    $element.find('li').on('click touchstart', function() {
+                        var link = $('a', this).attr('href');
+
+                        if(link) { 
+                            window.location.href = link;
+                        } else {
+                            if ($(this).is('.active')) {
+                                $(this).removeClass('active');
+                                $('ul', this).slideUp();
+                            } else {
+                                $('#sidebar-menu li').removeClass('active');
+                                $('#sidebar-menu li ul').slideUp();
+                                
+                                $(this).addClass('active');
+                                $('ul', this).slideDown();
+                            }
+                        }
+                    });
+                    
+                     $log.log('Sidebar Menu is ready');
+                });
+                
             }
         };
     }]);
 
-    weberp.directive('wMenuSubitem', [function () {
+    weberp.directive('wMenuSection',[function(){
+        return {
+          restrict:'E',
+          replace:true,
+          transclude:true,
+          require:'^wSidebarMenu',
+          controller:function($scope){},
+          template:'<div class="menu_section" ng-transclude></div>' 
+        };
+    }]);
+    
+    weberp.directive('wMenuItem', [function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            require: '^wMenuSection',
+            controller:function($scope){
+                
+            },
+            template:'<ul class="nav" ng-class="{\'child_menu\': menu.childs && menu.childs.length>0}"><li ng-repeat="child in menu.childs"><w-menu-subitem "></w-menu-subitem></li></ul>',
+            
+        };
+    }]);
+
+    weberp.directive('wMenuSubitem', ['$log','$compile',function ($log,$compile) {
         return {
             restrict: 'E',
             replace: true,
             require: '^wMenuItem',
-            transclude:true,
-            template:function($element,$attrs){
-                if (angular.isDefined($attrs.state) && $attrs.state !== "")
-                    return '<a ui-sref="' + $attrs.state + '" ng-transclude></a>';
-                if (angular.isDefined($attrs.link) && $attrs.link !== "")
-                    return '<a ng-click="' + $attrs.link + '" ng-transclude></a>';
-                return '<a ng-transclude></a>';
-            },
+
+            link:function ($scope,$element,$attrs) {
+                var elt=angular.element('<a>{{child.text}}<span ng-if="child.label" class="label label-{{child.label.variation}} pull-right">{{child.label.text}}</span></a>');
+                if (angular.isDefined($scope.child.state) && $scope.child.state !== "")
+                    elt.attr('ui-sref',$scope.child.state);
+                if (angular.isDefined($scope.child.link) && $scope.child.link !== "")
+                    elt.attr('ng-href', $scope.child.link);
+                if (angular.isDefined($scope.child.label)){
+                    var label=angular.element('')
+                }
+                    
+                var c=$compile(elt)($scope);
+                
+                $element.replaceWith(c);
+            }
            
         };
     }]);
-})(window,angular);
+    
+    weberp.directive('wSidebarFooter',[function(){
+        return{
+          restrict: 'E',
+          replace:true,
+          transclude:true,
+          template:'<div class="sidebar-footer hidden-small" ng-transclude></div>' ,
+          controller:function($scope){},
+          link:function($scope,$element,$attrs){}
+        };
+    }]);
+    
+    weberp.directive('wSidebarFooterItem',['$compile',function($compile){
+        return {
+          restrict: 'E',
+          replace:true,
+          require:'^wSidebarFooter',
+          scope:{
+              title:'@',
+              icon:'@'
+          },
+          controller:function($scope){},
+          link:function($scope,$element,$attrs){
+              var elt=angular.element('<a data-toggle="tooltip" data-placement="top" title="{{ title | translate }}"><span class="glyphicon glyphicon-{{icon}}" aria-hidden="true"></span></a>');
+              $scope.title=$attrs.title;
+              $scope.icon=$attrs.icon;
+              var c=$compile(elt)($scope);
+              $element.replaceWith(c);
+          }
+          
+        };
+    }]);
+    
+    
+    weberp.directive('wTopbarMenu',[function(){
+        return{
+          restrict:'E',
+          replace:true,
+          transclude:true,
+          template:'<div class="top_nav"><div class="nav_menu"><nav class="" role="navigation"><div class="nav toggle"><a id="menu_toggle"><i class="fa fa-bars"></i></a></div><ul class="nav navbar-nav navbar-right" ng-transclude></ul></nav></div></div>'  
+        };
+    }]);
+})(window,angular,$);
--- demo.js
+++ demo.js
@@ -1,19 +1,93 @@
 (function (window,angular) {
     'use strict';
     var weberp = window.weberp;
-    weberp.controller('main',['$scope',function($scope){
-        $scope.title="TITRE";
+    weberp.config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/home");
+         $stateProvider
+        .state('state1', {
+            url: "/state1",
+            templateUrl: "partials/state1.html"
+         })
+         .state('home',{
+             url:'/home',
+             template:'<div>home</div>'
+             
+         });
         
-        $scope.items=[
+    });
+    
+    
+    weberp.controller('main',['$scope',function($scope){
+        $scope.appTitle="Web Erp";
+        $scope.appIcon='pagelines';
+        $scope.menuSections=[{
+                title:'GENERAL',
+                menus:[
+                     {
+                        icon:'home',
+                        text:'Accueil',
+                        childs:[
+                            {
+                                id:0,
+                                text:'accueil 1',
+                                state:'state1'
+                            },
+                            {
+                                id:1,
+                                text:'accueil 2',
+                                link:'state2'
+                            },
+                        ]
+                    },
+                    {
+                        icon:'edit',
+                        text:'Forms',
+                        childs:[
+                            {
+                                id:0,
+                                text:'accueil 3',
+                                state:'state1'
+                            },
+                            {
+                                id:1,
+                                text:'accueil 4',
+                                link:'state4'
+                            },
+                        ]
+                    }
+                ]
+            },{
+                title:'LIVE ON',
+                menus:[
+                    {
+                        icon:'bug',
+                        text:'Additional Pages',
+                        childs:[{
+                            text:'E-commerce',
+                            label:{
+                                variation:'success',
+                                text:10
+                            }
+                            
+                        },{
+                            text:'Projects'
+                        }
+                        ]
+                    }
+                ]
+            }];/*
+            
             {
                 icon:'home',
                 text:'Accueil',
                 items:[
                     {
+                        id:0,
                         text:'accueil 1',
                         state:'state1'
                     },
                     {
+                        id:1,
                         text:'accueil 2',
                         link:'state2'
                     },
@@ -23,7 +97,7 @@
                 icon:'edit',
                 text:'Forms'
             }
-        ];
+        ];*/
     }])
     
   
